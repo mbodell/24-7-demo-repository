75cb5112502dee26f07a902245a31a820b526973
Michael Sandrof <sandrof@voxify.com>
2012-04-25 15:58:49 -0700

    1.0.2: includes support for email/texting, barcode scanning, contact/address book lookup and logging to native app log.

--- js/bridge.js
+++ js/bridge.js
@@ -1,19 +1,23 @@
 // This code is copyright 24/7, Inc. and is fully protected under copyright law.
 // This code may only be used pursuant to a valid license by 24/7, Inc.
+
 var _bridge_version = "1.0"
-var _bridge_build = 1;
+var _bridge_build = 2;
 
 function NativeBridgeClass() {
     this.state = new Object();
     this.state.prompts = new Array();
+    this.state.log = new Array();
     this.state.cancelPrompts = false;
-    this.state.grammar = "";
+    this.state.grammar = null;
     this.state.startRecognition = false;
-    this.state.message = "";
+    this.state.message = null;
     this.state.recoParams = null;
     this.state.getLocation = false;
-    this.state.eventsFrom = -1;
-    this.state.eventsTo = -1;
+    this.state.events = null;
+    this.state.sendMessage = null;
+    this.state.contactName = null;
+    this.state.scanBarcode = null;
 
     this.stateChanged = false;
     this.grammarCallback = null;
@@ -21,6 +25,8 @@ function NativeBridgeClass() {
     this.eventsCallback = null;
     this.initCallback = null;
     this.notificationCallback = null;
+    this.contactsCallback = null;
+    this.barcodeCallback = null;
 }
 
 NativeBridgeClass.prototype.onInitialize = function(callback) {
@@ -30,12 +36,27 @@ NativeBridgeClass.prototype.onInitialize = function(callback) {
 NativeBridgeClass.prototype.onNotification = function(callback) {
     this.notificationCallback = callback;
 };
+
+NativeBridgeClass.prototype.scanBarcode = function(params, callback) {
+    this.stateChanged = true;
+    this.state.scanBarcode = new Object();
+    this.state.scanBarcode.params = params;
+    this.barcodeCallback = callback;					
+}
   
 NativeBridgeClass.prototype.startRecognition = function() {
     this.stateChanged = true;
     this.state.startRecognition = true;
 };
   
+NativeBridgeClass.prototype.log = function(msg) {
+    this.stateChanged = true;
+    if (typeof msg == 'string')
+      this.state.log.push(msg);				
+    else
+      this.state.log.push(JSON.stringify(msg));				
+};
+
 NativeBridgeClass.prototype.playAudio = function(prompt) {
     this.stateChanged = true;
     this.state.prompts.push(prompt);
@@ -65,12 +86,12 @@ NativeBridgeClass.prototype.cancelAudio = function() {
 
 NativeBridgeClass.prototype.setMessage = function(t) {
     this.stateChanged = true;
-    this.state.message = t == null ? "" : t;  		 
+    this.state.message = t;
 };
 
 NativeBridgeClass.prototype.setGrammar = function(g, p, callback) {
     this.stateChanged = true;
-    this.state.grammar = g == null ? "" : g;
+    this.state.grammar = g;
     this.state.recoParams = p;
     this.grammarCallback = callback;
 };
@@ -83,9 +104,39 @@ NativeBridgeClass.prototype.getLocation = function(callback) {
 
 NativeBridgeClass.prototype.getEvents = function(from, to, callback) {
     this.stateChanged = true;
+    this.state.events = new Object();
+    this.state.events.from = from;
+    this.state.events.to = to;
     this.eventsCallback = callback;
-    this.state.eventsFrom = from;
-    this.state.eventsTo = to;
+};
+
+NativeBridgeClass.prototype.getContacts = function(name, callback) {
+    this.stateChanged = true;
+    this.contactsCallback = callback;
+    this.state.contactName = name;
+};
+
+NativeBridgeClass.prototype.sendMail = function(recipients, subject, body) {
+    this.stateChanged = true;
+    this.state.sendMessage = new Object();
+    this.state.sendMessage.type = "mail";
+    if (typeof recipients === "string")
+      this.state.sendMessage.recipients = [ recipients ];
+    else
+      this.state.sendMessage.recipients = recipients;
+    this.state.sendMessage.subject = subject;
+    this.state.sendMessage.body = body;
+};
+  
+NativeBridgeClass.prototype.sendText = function(recipients, body) {
+    this.stateChanged = true;
+    this.state.sendMessage = new Object();;
+    this.state.sendMessage.type = "text";
+    if (typeof recipients === "string")
+      this.state.sendMessage.recipients = [ recipients ];
+    else
+      this.state.sendMessage.recipients = recipients;
+    this.state.sendMessage.body = body;
 };
   
   // The functions below are intended to be invoked only from the Native side. 
@@ -94,16 +145,17 @@ NativeBridgeClass.prototype._initialize = function(o) {
     if (this.initCallback != null) {
       var _this = this;
       setTimeout(function(){
-	o.bridge = new Object();
+        o.bridge = new Object();
         o.bridge.version = _bridge_version;
-	o.bridge.build = _bridge_build;
+        o.bridge.build = _bridge_build;
         _this.initCallback(o);
       }, 1);
     }
 };
   
 NativeBridgeClass.prototype._setGrammarResult = function(result) {
-    this.state.grammar = "";
+    this.stateChanged = true;
+    this.state.grammar = null;
     this.state.recoParams = null;
     if (this.grammarCallback != null) {
       var _this = this;
@@ -131,6 +183,15 @@ NativeBridgeClass.prototype._setEvents = function(result) {
     }
 };
 
+NativeBridgeClass.prototype._setContacts = function(result) {
+    if (this.contactsCallback != null) {
+      var _this = this;
+      setTimeout(function(){
+        _this.contactsCallback(result);
+      }, 1);
+    }
+};
+
 NativeBridgeClass.prototype._setNotification = function(result) {
     if (this.notificationCallback != null) {
       var _this = this;
@@ -140,22 +201,32 @@ NativeBridgeClass.prototype._setNotification = function(result) {
     }
 };
 
+NativeBridgeClass.prototype._setBarcode = function(result) {
+    if (this.barcodeCallback != null) {
+      var _this = this;
+      setTimeout(function(){
+        _this.barcodeCallback(result);
+      }, 1);
+    }
+};
+
 NativeBridgeClass.prototype._getState = function() {
     if (this.stateChanged) {
       this.stateChanged = false;
       var ret = JSON.stringify(this.state);
       this.state.prompts.length = 0;
+      this.state.log.length = 0;
       this.state.startRecognition = false;
       this.state.cancelPrompts = false;
       this.state.getLocation = false;
-      this.state.eventsFrom = -1;
-      this.state.eventsTo = -1;
+      this.state.scanBarcode = null;
+      this.state.events = null;
+      this.state.contactName = null;
+      this.state.sendMessage = null;
       return ret;
     }
     return null;
 };
 
 var NativeBridge = new NativeBridgeClass();
-NativeBridge.initialize();
-
 
